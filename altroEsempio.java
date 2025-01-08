f8f33b89ff6fb512abf4f3e74b41f41fcc7ad794
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-08 11:21:35 +0100

    teoria in mattina

---
+++ altroEsempio.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class altroEsempio {
+   // args è un array di stringhe
+   public static void main(String[] args) {
+      System.out.println("Ciao");
+      System.out.print("Ciao");
+      System.out.println();
+      // system è una classe
+      double prezzo = 50.64;
+      System.out.println(prezzo);
+
+      // conversione
+      int prezzoInt = (int) prezzo;
+      System.out.println(prezzoInt);
+
+      String[] stagioni = new String[4];
+      stagioni[0] = "primavera";
+      stagioni[1] = "estate";
+
+      String[] mesi = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre",
+            "Ottobre", "Novembre", "Dicembre" };
+      System.out.println(Arrays.toString(mesi));
+
+      Scanner mioScanner = new Scanner(System.in);
+      System.out.println("Inserisci il tuo nome");
+      String nomeUtente = mioScanner.nextLine();
+      System.out.println("Quanti anni hai, " + nomeUtente + "?");
+      int anni = mioScanner.nextInt();
+      System.out.println("Ciao " + nomeUtente + " hai " + anni + " anni");
+
+   }
+}
